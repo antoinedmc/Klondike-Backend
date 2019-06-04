e4ab354420434f24003d3af6ef5b8332227fd04c
LudoCruchot <32196302+LudoCruchot@users.noreply.github.com>
2019-06-04 16:32:48 -0400

    clean

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello world !');
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-})
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+});
